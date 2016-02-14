4ccc80c7722a9eb188d77c18847b0ebc1a7438a6
Iqbal Fauzi <iqbalfawz@gmail.com>
2016-02-14 10:56:06 +0700

    First commit

---
+++ Camp.php
@@ -0,0 +1,336 @@
+<?php
+
+require_once dirname(__FILE__) . '/HTML5/Parser.php';
+
+class Camp
+{
+    /**
+     * The DOMDocument/HTML5 object
+     * @var object
+     */
+    private $doc;
+    
+    /**
+     * Original HTML of the content
+     * @var string
+     */
+    public $html;
+    
+    /**
+     * AMP version of the HTML content.
+     * @var string
+     */
+    public $amp;
+    
+    /**
+     * List of used amp component on the content.
+     * @var array
+     */
+    public $components = array();
+    
+    /**
+     * Download image to find image size
+     * @var boolean
+     */
+    public $downloadImage = false;
+    
+    /**
+     * local path to find the image for relative image src
+     * @var string
+     */
+    public $localImagePath = '';
+    
+    /**
+     * local hostname to download the image if the image not found on local path
+     * for image with relative src attribute
+     * @var string
+     */
+    public $localHost = '';
+    
+    /**
+     * Default image width size if no way to find image size.
+     * @var integer
+     */
+    public $defaultWidth = 300;
+    
+    /**
+     * Default image height size if no way to find image size.
+     * @var integer
+     */
+    public $defaultHeight = 200;
+    
+    /**
+     * Constructor
+     * @param string html The HTML content.
+     * @param array options List of options.
+     */
+    function __construct($html=null, $options=null){
+        if($html || $options)
+            $this->convert($html, $options);
+    }
+    
+    /**
+     * Add new component to the list
+     * @param string comp The component name.
+     * @return $this
+     */
+    private function _addComponent($comp){
+        if(!in_array($comp, $this->components))
+            $this->components[] = $comp;
+        return $this;
+    }
+    
+    /**
+     * Clean all prohibited tags
+     * @return $this
+     */
+    private function _cleanProhibitedTag(){
+        $tags = array(
+            'script',   // except type is `application/ld+jxon`.
+            'base',
+            'frame',
+            'frameset',
+            'object',
+            'param',
+            'applet',
+            'embed',
+            'form',
+            'input',    // except type is `button`.
+            'textarea',
+            'select',
+            'option',
+            'style',    // accepted only on head tag.
+            'link',     // accepted if rel is `canonical`.
+            'meta',     // only if with attribute `http-equiv`.
+            'a',        // if href start with `javascript:` and target is not `_blank`.
+//             'svg'       // TODO
+        );
+        
+        foreach($tags as $tag){
+            $els = $this->doc->getElementsByTagName($tag);
+            
+            if(!$els->length)
+                continue;
+            
+            for($i=($els->length-1); $i>=0; $i--){
+                $el = $els->item($i);
+                
+                if($tag == 'script'){
+                    if($el->getAttribute('type') == 'application/ld+json')
+                        continue;
+                }elseif($tag == 'input'){
+                    if($el->getAttribute('type') == 'button')
+                        continue;
+                }elseif($tag == 'link'){
+                    if($el->getAttribute('rel') == 'canonical')
+                        continue;
+                }elseif($tag == 'meta'){
+                    if(!$el->hasAttribute('http-equiv'))
+                        continue;
+                }elseif($tag == 'a'){
+                    if(substr($el->getAttribute('href'), 0, 11) == 'javascript:'){
+                        if(strtolower($el->getAttribute('target')) == '_blank')
+                            continue;
+                    }else{
+                        continue;
+                    }
+                }
+                
+                $el->parentNode->removeChild($el);
+            }
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Convert various ad to amp-ad 
+     * @return $this
+     */
+    private function _convertAd(){
+        // gads using ins
+        $inses = $this->doc->getElementsByTagName('ins');
+        if($inses->length){
+            for($i=($inses->length-1); $i>=0; $i--){
+                $ins = $inses->item(0);
+                if($ins->getAttribute('class') != 'adsbygoogle')
+                    continue;
+                
+                $attr = $this->_getAttribute($ins, array(
+                    'data-ad-client' => true,
+                    'data-ad-slot'   => true
+                ));
+                
+                if(!$attr['data-ad-client'] || !$attr['data-ad-slot'])
+                    continue;
+                
+                $amp_ad = $this->doc->createElement('amp-ad');
+                $attr['type'] = 'adsense';
+                $attr['height'] = 200;
+                $attr['width'] = 300;
+                
+                $this->_setAttribute($amp_ad, $attr);
+                $ins->parentNode->replaceChild($amp_ad, $ins);
+            }
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Convert img tag to amp-img/amp-anim
+     * @return $this
+     */
+    private function _convertImg(){
+        $imgs = $this->doc->getElementsByTagName('img');
+        if(!$imgs->length)
+            return $this;
+        
+        while($imgs->length){
+            $img = $imgs->item(0);
+            $attr= $this->_getAttribute($img, array(
+                'src'    => true,
+                'width'  => true,
+                'height' => true,
+                'alt'    => false
+            ));
+            
+            if(!$attr['width'] || !$attr['height']){
+                $is_relative = substr($attr['src'], 0, 4) !== 'http';
+                $download_it = true;
+                
+                // let see if it's using relative path
+                if($is_relative){
+                    if($this->localImagePath){
+                        $img_local = chop($this->localImagePath, '/') . '/';
+                        $img_local.= ltrim($attr['src'], '/');
+                        
+                        if(file_exists($img_local)){
+                            list($attr['width'], $attr['height']) = getimagesize($img_local);
+                            $download_it = false;
+                        }
+                    }
+                }
+                
+                // look like we need to download it.
+                if($download_it && $this->downloadImage){
+                    $img_src = $attr['src'];
+                    if($is_relative && $this->localHost)
+                        $img_src = chop($this->localHost, '/') . '/' . ltrim($img_src, '/');
+                    
+                    // let download the image
+                    if(filter_var($img_src, FILTER_VALIDATE_URL)){
+                        $img_file = tempnam(sys_get_temp_dir(), "camp");
+                    
+                        $cu = curl_init($img_src);
+                        curl_setopt($cu, CURLOPT_RETURNTRANSFER, 1);
+                        curl_setopt($cu, CURLOPT_FOLLOWLOCATION, 1);
+                        
+                        $bin = curl_exec($cu);
+                        curl_close($cu);
+                        
+                        $f = fopen($img_file, 'w');
+                        fwrite($f, $bin);
+                        fclose($f);
+                        
+                        list($attr['width'], $attr['height']) = getimagesize($img_file);
+                        unlink($img_file);
+                    }
+                }
+            }
+            
+            if(!$attr['width'])
+                $attr['width'] = $this->defaultWidth;
+            if(!$attr['height'])
+                $attr['height']= $this->defaultHeight;
+            
+            // TODO findout how to use different layout type
+            $attr['layout'] = 'responsive';
+            
+            $tag = preg_match('!\.gif$!', $attr['src']) ? 'amp-anim' : 'amp-img';
+            $amp_img = $this->doc->createElement($tag);
+            $this->_setAttribute($amp_img, $attr);
+            
+            if($tag == 'amp-anim')
+                $this->_addComponent($tag);
+            
+            $img->parentNode->replaceChild($amp_img, $img);
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Get element attribute
+     * @param object node The node where the attr taken.
+     * @param array attrs attr-required pair of attribute to get.
+     * @return array name-value pair of the element attribute
+     */
+    private function _getAttribute($node, $attrs){
+        $result = array();
+        foreach($attrs as $attr => $must){
+            $value = $node->getAttribute($attr);
+            if($node->hasAttribute($attr) || $must)
+                $result[$attr] = $value;
+        }
+        
+        return $result;
+    }
+    
+    /**
+     * Set element attribute
+     * @param object node The node where the element will be put
+     * @param array attrs name-value pair of attribute to set.
+     */
+    private function _setAttribute(&$node, $attrs){
+        foreach($attrs as $att => $value)
+            $node->setAttribute($att, $value);
+    }
+    
+    /**
+     * Parse the HTML text.
+     * @param string html The html content to convert.
+     * @param array options List of convertion 
+     * @return $this
+     */
+    public function convert($html=null, $options=null){
+        if($options)
+            $this->setOptions($options);
+        if($html)
+            $this->html = $html;
+        
+        $this->amp = null;
+        $this->components = array();
+        
+        if(!$this->html)
+            return $this;
+            
+        $html = '<!DOCTYPE html><html><body>' . $this->html . '</body></html>';
+        $this->doc = HTML5_Parser::parse($html);
+        
+        $this
+            ->_convertImg()
+            ->_convertAd()
+            ->_cleanProhibitedTag();
+        
+        $amp = $this->doc->saveHTML();
+        preg_match('!^.+<body>(.+)</body>.+$!s', $amp, $m);
+        $this->amp = $m[1];
+        
+        return $this;
+    }
+    
+    /**
+     * Set option(s)
+     * @param string|array key The options key or list of option-value pair
+     * @param mixed value The option value, only if $key is string
+     * @return $this
+     */
+    public function setOptions($key, $value=null){
+        if(!is_array($key))
+            $key = array($key=>$value);
+        
+        foreach($key as $name => $value)
+            $this->$name = $value;
+    }
+}
---
+++ test/CampTest.php
@@ -0,0 +1,234 @@
+<?php
+
+require dirname(dirname(__FILE__)) . '/Camp.php';
+
+class CampTest extends PHPUnit_Framework_TestCase
+{
+    /**************************************************************************
+     * AMP-IMG | AMP-ANIM
+     **************************************************************************/
+    
+    public function ampImgProvider(){
+        return array(
+        
+            // amp-img
+            'should convert `img` tag to `amp-img`' => array(
+                'lorem <img src="lorem.png" width="200" height="100" alt="..."> ipsum',
+                'lorem <amp-img src="lorem.png" width="200" height="100" alt="..." layout="responsive"></amp-img> ipsum'
+            ),
+                
+            'should convert img to amp-img without alt' => array(
+                'lorem <img src="lorem.jpg" width="250" height="150"> ipsum',
+                'lorem <amp-img src="lorem.jpg" width="250" height="150" layout="responsive"></amp-img> ipsum'
+            ),
+                
+            'should get image size from local file system' => array(
+                'lorem <img src="/static/100x150.jpg"> ipsum',
+                'lorem <amp-img src="/static/100x150.jpg" width="100" height="150" layout="responsive"></amp-img> ipsum',
+                array('localImagePath'=>dirname(__FILE__))
+            ),
+                
+            'should get image size from local host' => array(
+                'lorem <img src="150x150.jpg"> ipsum',
+                'lorem <amp-img src="150x150.jpg" width="150" height="150" layout="responsive"></amp-img> ipsum',
+                array('downloadImage' => true, 'localHost' => 'https://placehold.it')
+            ),
+                
+            'should get image size from different host' => array(
+                'lorem <img src="https://placehold.it/200x150.jpg"> ipsum',
+                'lorem <amp-img src="https://placehold.it/200x150.jpg" width="200" height="150" layout="responsive"></amp-img> ipsum',
+                array('downloadImage' => true)
+            ),
+                
+            'should use default image size' => array(
+                'lorem <img src="/100x50.jpg"> ipsum',
+                'lorem <amp-img src="/100x50.jpg" width="100" height="50" layout="responsive"></amp-img> ipsum',
+                array('downloadImage' => false, 'defaultWidth'=>100, 'defaultHeight'=>50)
+            ),
+            
+            // amp-anim
+            'should convert img to amp-anim on gif file' => array(
+                'lorem <img src="lorem.gif" width="350" height="150" alt=""> ipsum',
+                'lorem <amp-anim src="lorem.gif" width="350" height="150" alt="" layout="responsive"></amp-anim> ipsum'
+            )
+        );
+    }
+    
+    /**
+     * @dataProvider ampImgProvider
+     * @group amp-image
+     */
+    public function testAmpImg($html, $amp, $options=array()){
+        $camp = new Camp($html, $options);
+        $this->assertEquals($amp, $camp->amp);
+    }
+    
+    /**
+     * @group amp-image
+     */
+    public function testAmpAnim(){
+        $html = 'lorem <img src="lorem.gif" width="350" height="150" alt=""> ipsum';
+        $camp = new Camp($html);
+        
+        $this->assertContains('amp-anim', $camp->components);
+    }
+    
+    /**************************************************************************
+     * AMP-AD
+     **************************************************************************/
+     
+     public function ampAdProvider(){
+        return array(
+            array(
+                'lorem <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><!-- MRSNavResponsive --><ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3216619137195954" data-ad-slot="9167553021" data-ad-format="auto"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script> ipsum',
+                'lorem <!-- MRSNavResponsive --><amp-ad data-ad-client="ca-pub-3216619137195954" data-ad-slot="9167553021" type="adsense" height="200" width="300"></amp-ad> ipsum'
+            )
+        );
+     }
+     
+     /**
+      * @dataProvider ampAdProvider
+      * @group amp-ad 
+      */
+    public function testAmpAd($html, $amp){
+        $camp = new Camp($html);
+        $this->assertEquals($amp, $camp->amp);
+    }
+    
+    /**************************************************************************
+     * PROHIBITED TAG
+     **************************************************************************/
+     
+    public function prohibitedProvider(){
+        return array(
+            'should remove script attribute' => array(
+                'lorem <script>alert("a");</script> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should leave script with type application/ld+json' => array(
+                'lorem <script type="application/ld+json"></script> ipsum',
+                'lorem <script type="application/ld+json"></script> ipsum'
+            ),
+            
+            'should remove base tag' => array(
+                'lorem <base href="http://www.w3schools.com/images/" target="_blank"> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove frame tag' => array(
+                'lorem <frame src="frame_a.htm"> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove frameset tag' => array(
+                'lorem <frameset cols="100%"><frame src="frame_a.htm"></frameset> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove object tag' => array(
+                'lorem <object width="400" height="400" data="helloworld.swf"></object> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove param tag' => array(
+                'lorem <param name="autoplay" value="true"> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove applet tag' => array(
+                'lorem <applet code="Bubbles.class" width="350" height="350">Java applet that draws animated bubbles.</applet> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove embed tag' => array(
+                'lorem <embed src="helloworld.swf"> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove form tag' => array(
+                'lorem <form method="post"></form> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove input tag' => array(
+                'lorem <input type="text"> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should leave input type button' => array(
+                'lorem <input type="button"> ipsum',
+                'lorem <input type="button"> ipsum'
+            ),
+            
+            'should remove input tag but leave input type button tag' => array(
+                'lorem <input type="button"><input type="text"> ipsum',
+                'lorem <input type="button"> ipsum'
+            ),
+            
+            'should remove textarea tag' => array(
+                'lorem <textarea></textarea> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove select tag' => array(
+                'lorem <select><option value="a"></option></select> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove option tag' => array(
+                'lorem <option value="a"></option> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove style tag' => array(
+                'lorem <style>.body{background-color:white}</style> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should remove link tag' => array(
+                'lorem <link rel="stylesheet" href="css.css"> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should leave link tag with rel canonical' => array(
+                'lorem <link rel="canonical" href="blog.web"> ipsum',
+                'lorem <link rel="canonical" href="blog.web"> ipsum'
+            ),
+            
+            'should leave meta tag' => array(
+                'lorem <meta name="lorem" content="ipsum"> ipsum',
+                'lorem <meta name="lorem" content="ipsum"> ipsum'
+            ),
+            
+            'should remove meta tag with http-equiv tag' => array(
+                'lorem <meta http-equiv="X-UA-Compatible" content="IE=edge"> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should leave a tag' => array(
+                'lorem <a href="lorem">click</a> ipsum',
+                'lorem <a href="lorem">click</a> ipsum'
+            ),
+            
+            'should remove a tag with href start with javascript:' => array(
+                'lorem <a href="javascript:alert(\'a\')">click</a> ipsum',
+                'lorem  ipsum'
+            ),
+            
+            'should leave a tag with href javascript and target _blank' => array(
+                'lorem <a href="javascript:alert(\'a\')" target="_blank">click</a> ipsum',
+                'lorem <a href="javascript:alert(\'a\')" target="_blank">click</a> ipsum'
+            )
+        );
+    }
+     
+    /**
+     * @dataProvider prohibitedProvider
+     * @group prohibited-tag
+     */
+    public function testProhibitedTags($html, $amp){
+        $camp = new Camp($html);
+        $this->assertEquals($amp, $camp->amp);
+    }
+}
